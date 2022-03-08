9cd0c94fc63e1026c33889ae4a20042462062b47
lm <f10408034@gmail.com>
2022-03-08 11:39:37 +0800

    enum for Game State design 2022/03/08

---
+++ app/src/main/java/com/vangoog/guessnumber/NumberGame.kt
@@ -0,0 +1,32 @@
+package com.vangoog.guessnumber
+
+class NumberGame {
+
+    enum class GameState {
+        INIT, BIGGER, SMALLER, BINGO, END
+    }
+
+    var secret = 0
+    var counter = 0
+    var end = false
+    init {
+        reset()
+    }
+    fun reset(){
+
+        secret = (1..10).random()
+        counter = 0
+        end = false
+    }
+
+    fun guess(num: Int): GameState{
+        counter++
+        val message = if (num > secret) GameState.SMALLER
+        else if (num < secret) GameState.BIGGER
+            else {
+            end =true
+            GameState.BINGO
+        }
+        return message
+    }
+}
